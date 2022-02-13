90652c3d3015a1eea1995139a78b332c468b7c94
Will Neal <willwneal@gmail.com>
2022-02-13 18:48:02 -0500

    Wrote function to generate Manager card and to write it to the generated HTML file

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const managerQuestions = [
         type: "input",
         message: "Enter the team manager's name:"
     },
-    ,
     {
         name: "id",
         type: "input",
@@ -32,7 +31,7 @@ const menuQuestion = [
         name: "employee",
         type: "list",
         message: "what type of employee would you like to add?",
-        choices = ["Engineer", "Intern", "None"]
+        choices: ["Engineer", "Intern", "None"],
     }
 ]
 
@@ -82,30 +81,6 @@ const internQuestions = [
     },
 ]
 
-
-
-let managerCard = generateManager((userAnswers) => {
-    return `
-    <div class="card" style="width: 18rem; border: 0px; border: 3px solid lightgrey; margin: 30px">
-            <div style="padding: 15px; border-radius: 2px 2px 0px 0px;" class="card-body bg-primary">
-                <div style="text-align: center" class="header-container bg-primary text-light">
-                    <h5 class="card-title bg-primary text-light">${userAnswers.name}</h5>
-                    <h6 class="card-subtitle mb-2 bg-primary text-light">Manager</h6>
-                </div>
-                
-            </div>
-            <div style="margin-left: 20px; margin-right: 20px; margin-top: 35px; margin-bottom: 35px; border: 1px solid lightgray;" class="info-container">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${userAnswers.id}</li>
-                    <li class="list-group-item">Email: ${userAnswers.email}</li>
-                    <li class="list-group-item">Office #: ${userAnswers.office}</li>
-              </ul>
-            </div>  
-          </div>
-    `
-})
-
-
 //function to render the HTML page
 let employeeCard = "";
 function renderHTML(data) {
@@ -136,13 +111,58 @@ function renderHTML(data) {
     `
 }
 
+
+function generateManager(userAnswers) {
+    return `
+    <div class="card" style="width: 18rem; border: 0px; border: 3px solid lightgrey; margin: 30px">
+            <div style="padding: 15px; border-radius: 2px 2px 0px 0px;" class="card-body bg-primary">
+                <div style="text-align: center" class="header-container bg-primary text-light">
+                    <h5 class="card-title bg-primary text-light">${userAnswers.name}</h5>
+                    <h6 class="card-subtitle mb-2 bg-primary text-light">Manager</h6>
+                </div>
+                
+            </div>
+            <div style="margin-left: 20px; margin-right: 20px; margin-top: 35px; margin-bottom: 35px; border: 1px solid lightgray;" class="info-container">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${userAnswers.id}</li>
+                    <li class="list-group-item">Email: ${userAnswers.email}</li>
+                    <li class="list-group-item">Office #: ${userAnswers.office}</li>
+              </ul>
+            </div>  
+          </div>
+    `
+}
+
+
+
+
 //Function to begin generating webpage
 function generateProfile() {
     inquirer.prompt(managerQuestions)
     .then ((answers) => {
-        generateManager(answers)
-        managerCard
+        managerCard = generateManager(answers)
+        console.log("Manger card: " + managerCard)
+        employeeCard += managerCard
+        inquirer.prompt(menuQuestion)
+        .then ((menuQuestion) => {
+            if (menuQuestion.employee === "Engineer") {
+                console.log("You have selected Engineer")
+            } else if (menuQuestion.employee === "Intern") {
+                console.log("You have selected Intern")
+            } else {
+                fs.writeFile("test.html", renderHTML(employeeCard), (err) => {
+                    if(err) {
+                        console.error(err);
+                        return;
+                    } 
+
+                    console.log("File written successfully")
+                })
+            }
+        })
+        
     })
+    
 }   
 
 generateProfile()
