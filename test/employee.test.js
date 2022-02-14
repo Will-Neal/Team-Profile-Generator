613ea242f7b004bac0080db1c77bcfe50cc36875
Will Neal <willwneal@gmail.com>
2022-02-14 17:30:19 -0500

    wrote tests to check all aspects of the employee class

--- test/employee.test.js
+++ test/employee.test.js
@@ -3,10 +3,28 @@ const Employee = require('../lib/employee')
 describe("Employee Class", () => {
     describe('name property creation', () => {
         it("should create a property with the name value passed in", ()=>{
-            const employee = new Employee("Tom", "Manager", "1", "tom@management.com");
-        
+            const employee = new Employee("Tom", "Manager", 1, "tom@management.com");
             expect(employee.name).toBe("Tom")
-        })
+        });
         
+    });
+    describe('title property creation', () => {
+        it("should create a property with the title property passed in", () => {
+            const employee = new Employee("Larry", "Engineer", 2, "larry@engineering.com");
+            expect(employee.title).toBe("Engineer")
+        });
+
+    })
+    describe('id property creation', () => {
+        it("should create a property with the id number passed in", () => {
+            const employee = new Employee("Sasha", "Intern", 3, "sasha@unpaidintern.com");
+            expect(employee.id).toBe(3)
+        })
+    })
+    describe('email property creation', () => {
+        it("should create a property with the email address passed in", () => {
+            const employee = new Employee("Lena", "Manager", 4, "lena@uppermanagement.com");
+            expect(employee.email).toBe("lena@uppermanagement.com")
+        })
     })
 })
